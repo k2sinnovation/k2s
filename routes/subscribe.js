58bcdfee456d670cbd49f2c197799b5a84406095
k2sinnovation <kassime.karime@gmail.com>
2025-07-18 19:37:43 +0200

    subscribe.js

--- routes/subscribe.js
+++ routes/subscribe.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
+const User = require("../models/usermodel");
 
 router.post("/upgrade", async (req, res) => {
   const { user_id, new_level } = req.body;
