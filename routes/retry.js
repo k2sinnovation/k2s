d7f3e91cce058840441a38af2b6d6cd79d01c0b3
k2sinnovation <kassime.karime@gmail.com>
2025-07-19 00:59:26 +0200

    retry.js

--- routes/retry.js
+++ routes/retry.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { retryFinalDiagnosis } = require('../controllers/retryController');
+
+router.post('/', retryFinalDiagnosis);
+
+module.exports = router;
 
