b2e05fb054cc1b41b6332a4cbde34f8633d2a7ee
k2sinnovation <kassime.karime@gmail.com>
2025-07-20 03:33:10 +0200

    analyze.js

--- routes/analyze.js
+++ routes/analyze.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { askOpenAI } = require("../controllers/openaiService");
 const { buildFirstAnalysisPrompt } = require("../utils/promptBuilder");
+const prompt = buildFirstAnalysisPrompt(text);
+const response = await askOpenAI(prompt, text);
 
 router.post("/", async (req, res) => {
   try {
@@ -10,9 +12,6 @@ router.post("/", async (req, res) => {
     //if (!text || text.length < 80) {
      // return res.status(400).json({ error: "Demande trop courte ou invalide." });
     //}
-
-    const prompt = buildFirstAnalysisPrompt(text);
-    const response = await askOpenAI(prompt, text);
 
     res.json({ questions: response });
   } catch (error) {
