e5e4dd0134756a8280b4c5d43816c80465916f16
k2sinnovation <kassime.karime@gmail.com>
2025-07-20 05:00:09 +0200

    analyze.js

--- routes/analyze.js
+++ routes/analyze.js
@@ -1,23 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { askOpenAI } = require("../controllers/openaiService");
-const { buildFirstAnalysisPrompt } = require("../utils/promptBuilder");
-const prompt = buildFirstAnalysisPrompt(text);
-const response = await askOpenAI(prompt, text);
 
-router.post("/", async (req, res) => {
-  try {
-    const { text } = req.body;
+const { analyzeRequest } = require("../controllers/analyzeController");
 
-    //if (!text || text.length < 80) {
-     // return res.status(400).json({ error: "Demande trop courte ou invalide." });
-    //}
-
-    res.json({ questions: response });
-  } catch (error) {
-    console.error("Erreur analyse :", error);
-    res.status(500).json({ error: "Erreur serveur" });
-  }
-});
+router.post("/", analyzeRequest);
 
 module.exports = router;
