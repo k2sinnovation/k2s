1dbd21d786d99ffd63c89f6da9dc3e788f51ec90
k2sinnovation <kassime.karime@gmail.com>
2025-07-29 16:04:52 +0200

    openaiService.js

--- controllers/openaiService.js
+++ controllers/openaiService.js
@@ -6,7 +6,7 @@ exports.askOpenAI = async (prompt, userText) => {
     const response = await axios.post(
       "https://api.openai.com/v1/chat/completions",
       {
-        model: "gpt-4o-mini", // plus rapide et économique
+        model: "gpt-4", // plus rapide 
         messages: [
           { role: "system", content: prompt },
           { role: "user", content: userText }
