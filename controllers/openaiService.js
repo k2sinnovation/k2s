36ab7872192e22161d11655eb5be80e748b9c7e4
k2sinnovation <kassime.karime@gmail.com>
2025-08-06 19:00:17 +0200

    openaiService.js

--- controllers/openaiService.js
+++ controllers/openaiService.js
@@ -8,7 +8,7 @@ exports.askOpenAI = async (prompt, userText) => {
     const response = await axios.post(
       "https://api.openai.com/v1/chat/completions",
       {
-        model: "gpt-3.5-turbo-1106", // plus rapide 
+        model: "chatgpt-4o-latest", // plus rapide 
         messages: [
           { role: "system", content: prompt },
           { role: "user", content: userText }
@@ -27,6 +27,7 @@ exports.askOpenAI = async (prompt, userText) => {
     throw new Error("Erreur OpenAI");
   }
 };
+
 
 
 
