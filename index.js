fa254b069b23535b608233d11aeff31352a6fb0e
k2sinnovation <kassime.karime@gmail.com>
2025-08-11 00:10:25 +0200

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const OpenAI = require("openai");
 const { generateTTS } = require('./controllers/openaiService'); // à créer pour la génération vocale
-const { promptTTSVocal } = require('./promptsTTSVocal');
+const { promptTTSVocal } = require('./utils/promptsTTSVocal');
 
 require('dotenv').config();
 
