67fdddd9225e8de623ed4868fd9063882e52a30d
k2sinnovation <kassime.karime@gmail.com>
2025-09-11 02:59:40 +0200

    assembly.js

--- routes/assembly.js
+++ routes/assembly.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { processAudioAndReturnJSONRealtime } = require('../controllers/realtimeService');
+const { processAudioAndReturnJSONRealtime } = require('../controllers/assemblyService');
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
