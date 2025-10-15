a0ca8f025e19da7177fe103bd93077628b15d769
k2sinnovation <kassime.karime@gmail.com>
2025-10-15 02:20:57 +0200

    User.js

--- service_ia/models/User.js
+++ service_ia/models/User.js
@@ -1,8 +1,8 @@
-
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
+// Vérifie si le modèle existe déjà
+const User = mongoose.models.User || mongoose.model('User', new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -41,18 +41,18 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
-});
+}));
 
 // Hash du mot de passe avant sauvegarde
-userSchema.pre('save', async function (next) {
+User.schema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 12);
   next();
 });
 
 // Méthode pour comparer les mots de passe
-userSchema.methods.comparePassword = async function (candidatePassword) {
+User.schema.methods.comparePassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = User;
